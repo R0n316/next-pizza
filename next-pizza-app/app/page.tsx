b95389c8cbcf0eba8a8540f5f687db57eb34f195
R0n316 <manyakinsasha@gmail.com>
2024-09-13 19:47:13 +0300

    layout top bar

--- next-pizza-app/app/page.tsx
+++ next-pizza-app/app/page.tsx
@@ -1,10 +1,12 @@
-import {Button} from "@/components/ui/button";
+import {Container, Title, TopBar} from "@/components/shared";
 
 export default function Home() {
   return (
-      <main>
-        <span>Home</span>
-        <Button variant={"outline"}>Корзина</Button>
-      </main>
+      <>
+          <Container className={'mt-10'}>
+              <Title text={'Все пиццы'} size={'lg'} className={'font-extrabold'} />
+          </Container>
+          <TopBar/>
+      </>
   );
 }
