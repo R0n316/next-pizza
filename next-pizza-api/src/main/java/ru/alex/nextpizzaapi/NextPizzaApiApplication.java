66a5d667e6e3ce4fb7b7b550bf9f3bc90c00a65d
R0n316 <manyakinsasha@gmail.com>
2024-09-15 14:19:55 +0300

    create spring boot project for api application

---
+++ next-pizza-api/src/main/java/ru/alex/nextpizzaapi/NextPizzaApiApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.nextpizzaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextPizzaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextPizzaApiApplication.class, args);
+	}
+
+}
